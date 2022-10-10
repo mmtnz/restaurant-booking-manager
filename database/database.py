ebe83ad2f482546015c79f66c8007ca1dc94aff6
mmtnz <mariomlafuente@gmail.com>
2022-10-10 17:48:26 +0200

    -add coments

--- database/database.py
+++ database/database.py
@@ -3,6 +3,10 @@ import mysql.connector as mysqlcon
 
 class DataBase:
 
+    """
+    DataBase class to control MySQL database
+    """
+
     def __init__(self, host, schema_name, user, password):
         self._host = host
         self._schema_name = schema_name
@@ -16,7 +20,11 @@ class DataBase:
         self.error_creating = False
         self._connect()
 
-    def _connect(self):
+    def _connect(self) -> None:
+        """
+        To connect to database
+        :return:
+        """
         num_attempts = 0
         while num_attempts < self._num_attempts:
 
@@ -30,15 +38,27 @@ class DataBase:
 
         self.error_creating = TimeoutError
 
-    def _disconnect(self):
+    def _disconnect(self) -> None:
+        """
+        To disconnect from database
+        :return:
+        """
         try:
             self._cursor.close()
             self._connection.close()
         except Exception as e:
             print(f'[!] Error disconnecting -> {type(e)}: {e}')
 
-    def select_query(self, table_name, columns=None, where_condition=None, order_by_column=None, order_asc=True):
-
+    def select_query(self, table_name, columns=None, where_condition=None, order_by_column=None, order_asc=True) -> list:
+        """
+        To execute SELECT SQl query
+        :param table_name:
+        :param columns:
+        :param where_condition: string with WHERE condition
+        :param order_by_column: ORDER BY sentence
+        :param order_asc: ASC or DESC only if order by
+        :return: list
+        """
         if self._connection is None or not self._connection.is_connected():
             self._connect()
         self._cursor = self._connection.cursor()
@@ -58,7 +78,14 @@ class DataBase:
         self._cursor.execute(sql_select_query)
         return self._cursor.fetchall()
 
-    def insert_query(self, table_name, values, columns=None):
+    def insert_query(self, table_name, values, columns=None) -> None:
+        """
+        To execute INSERT SQL command
+        :param table_name:
+        :param values: values to add
+        :param columns: columns where data is added
+        :return: None
+        """
         if not self._connection.is_connected():
             self._connect()
         self._cursor = self._connection.cursor()
@@ -70,7 +97,13 @@ class DataBase:
         self._cursor.execute(sql_insert_query)
         self._connection.commit()
 
-    def delete_query(self, table_name, condition):
+    def delete_query(self, table_name, condition) -> None:
+        """
+        To execute DELETE SQL command
+        :param table_name:
+        :param condition:
+        :return: None
+        """
         if not self._connection.is_connected():
             self._connect()
         self._cursor = self._connection.cursor()
@@ -79,8 +112,14 @@ class DataBase:
         self._cursor.execute(sql_delete_query)
         self._connection.commit()
 
-    def update_query(self, table_name, values_str, condition):
-
+    def update_query(self, table_name, values_str, condition) -> None:
+        """
+        To execute UPDATE SQL command
+        :param table_name:
+        :param values_str: values to update
+        :param condition:
+        :return: None
+        """
         if not self._connection.is_connected():
             self._connect()
         self._cursor = self._connection.cursor()
